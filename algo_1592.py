c1ffd5ee4ef8a19a299067ed4b0513fad5165d69
yerin <yerin0110@naver.com>
2021-08-30 22:24:12 +0900

    1592

---
+++ algo_1592.py
@@ -0,0 +1,13 @@
+ys_n, ys_m, ys_l = map(int, input().split())
+boals_count = []
+n_list = []
+boal_position = 0
+for i in range(ys_n):
+    n_list.append[i+1]
+
+while True:
+    if boals_count % 2 != 0:
+        boal_position +=2
+        boals_count[i] += 1
+    elif boals_count %2 == 0:
+        boal_position
