e40b3215437dcf1ccaa0e5d1a19e4c3e30248c6c
yerin <yerin0110@naver.com>
2021-09-06 19:26:07 +0900

    2798

--- algo_2798.py
+++ algo_2798.py
@@ -1,13 +1,26 @@
 howmany_cards, max_num = map(int, input().split())
-cards = list(map(int, input().split()))
-print_num = 0
-lst = []
+all_cards = list(map(int, input().split()))
+store_list = []
 
-for i in range(3):
-    lst[i] = cards[i]
+for i in range(howmany_cards):
+    for j in range(i+1,howmany_cards):
+        for k in range(j+1, howmany_cards):
+            sum_num = all_cards[i] + all_cards[j] + all_cards[k]
+            store_list.append(sum_num)
+            if howmany_cards ==3:
+                break
 
-if sum(lst) < 21:
-    for i in range(3):
-        
+#store_list = set(store_list)
+store_list2 = []
 
-print(max_num)
+for i in store_list:
+    if max_num >=i:
+        store_list2.append(i)
+
+
+num_max = 0
+for i in store_list2:
+    if num_max < i:
+        num_max = i
+ 
+print(num_max)
