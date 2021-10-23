8782187ac08346afb44bec119d1a2f96fe5db688
yerin <yerin0110@naver.com>
2021-10-23 17:22:49 +0900

    1158

--- algo_1158.py
+++ algo_1158.py
@@ -1,35 +1,17 @@
-N, K = map(int, input().split())
-josephus = [i for i in range(1,N+1)]
+from collections import deque
 
-count=0
+N,K = map(int,input().split())
+
+queue = deque()
+for i in range(1,N+1):
+    queue.append(i)
+print(queue)
+
 result = []
-while len(josephus):
-    a = josephus.pop(0)
-    count+=1
+count=0
 
-    if count<K:
-        josephus.append(a)
-
-    else:
-        result.append(a)
-        count=0
-    
-print("<", ', '.join(str(i) for i in result), ">", sep = '')
-
-# from collections import deque
-
-# queue = deque()
-# answer = []
-
-# N, K = map(int, input().split())
-
-# queue = [i for i in range(1, N+1)]
-
-# print(queue)
-
-# while queue:
-#     for i in range(K-1):
-#         queue.append(queue.popleft())
-#     answer.append(queue.popleft())
-# print("<", ", ".join(str(i) for i in answer), ">", sep="")
-
+while queue:
+    for i in range(K-1):
+        queue.append(queue.popleft())
+    result.append(queue.popleft())
+print("<", ", ".join(str(i) for i in result), ">" ,sep="")
