fbb7923953643b4a9379ee474b0283177f3ef211
yerin <yerin0110@naver.com>
2022-03-29 10:12:17 +0900

    완주하지못한선수

---
+++ algorithm_code/programmers_완주하지못한선수.py
@@ -0,0 +1,12 @@
+def solution(participant, completion):
+    answer = {}
+    for i in participant:
+        answer[i] = answer.get(i,0) + 1
+    for j in completion:
+        answer[j] -= 1
+    for k in answer:
+        if answer[k]:
+            return k
+
+a = solution(["leo", "kiki", "eden"], ["eden", "kiki"])
+print(a)
