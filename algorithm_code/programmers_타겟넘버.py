49866d5abfaea541b5f50a615fae80cce20165c4
yerin <yerin0110@naver.com>
2022-04-07 14:33:04 +0900

    타겟넘버

---
+++ algorithm_code/programmers_타겟넘버.py
@@ -0,0 +1,22 @@
+from itertools import permutations, product
+
+
+def solution(numbers, target):
+    answer = 0
+    store = [[]*2 for _ in range(len(numbers))]
+
+
+    for i in range(len(store)):
+        store[i]= numbers[i],-numbers[i]
+
+
+    # for i in range(len(store)*2):
+    a = list(product(*store))
+    for i in a:
+        if sum(i) == target:
+            answer+=1
+    
+        
+    return answer
+
+
