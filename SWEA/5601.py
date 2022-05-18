a9eb5c255e754a661beabf612c85fd52de917786
yerin <yerin0110@naver.com>
2022-05-19 07:26:30 +0900

    5601. [Professional] 쥬스 나누기

---
+++ SWEA/5601.py
@@ -0,0 +1,9 @@
+tc = int(input())
+for i in range(tc):
+    n = int(input())
+    one = 1
+    print("#", i+1,sep="",end=" ")
+
+    for j in range(n):
+        print(1, "/", n, end=" ",sep="")
+    print()
